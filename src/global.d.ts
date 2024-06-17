e84113c9c39d1172a5925472275a2fd8fccdccf3
Lúcio Andrade <lucioandradejr@gmail.com>
2024-06-17 14:07:51 -0400

    fix: typos

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,16 @@
 import "hono";
+import "typed-htmx";
 
 declare module "hono" {
   interface ContextRenderer {
     (content: string | Promise<string>, props?: { title?: string }):
       | Response
       | Promise<Response>;
+  }
+}
+
+declare module "hono/jsx" {
+  namespace JSX {
+    interface HTMLAttributes extends HtmxAttributes {}
   }
 }
