f594faa56605455c8b24fbf400c023919b4076e8
Lúcio Andrade <lucioandradejr@gmail.com>
2024-06-17 14:11:27 -0400

    feat: progressive enhancement

--- src/index.tsx
+++ src/index.tsx
@@ -43,7 +43,7 @@ app.get("/", (c) =>
           -
         </Button>
       </div>
-      <div id="container">
+      <div id="container" hx-boost="true">
         Clicked on button<span class="font-bold text-xl"> {counter} </span>
         {counter !== 1 ? "times" : "time"}
       </div>
