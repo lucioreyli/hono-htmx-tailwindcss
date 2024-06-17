86d599008406d8216d837194c0ff51fa2f2ffe34
Lúcio Andrade <lucioandradejr@gmail.com>
2024-06-16 21:06:13 -0400

    fix: typos

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,6 @@
 import { cn } from "@/lib/cn";
-import type { JSX } from "hono/jsx";
 
-export const Button = ({ children, ...props }: JSX.HTMLAttributes) => (
+export const Button = ({ children, ...props }: JSX.HtmlButtonTag) => (
   <button {...props} class={cn("p-6", props.class)}>
     {children}
   </button>
